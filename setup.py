cc4afccf0bbd05895dd303d0045d43037d756437
kozec <kozec@kozec.com>
2016-04-09 09:49:07 +0200

    Added/imported setup.py, so I can compile libuinput.so

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+uinput = Extension('libuinput',
+                   sources = ['scc/uinput.c'])
+
+setup(name='sccontroller',
+      version='1.0',
+      description='Standalone controller maping tool',
+      author='kozec',
+      package_dir={'scc': 'scc'},
+      packages=['scc'],
+      scripts=['scc.py'],
+      license='GPL2',
+      platforms=['Linux'],
+      ext_modules=[uinput, ])
