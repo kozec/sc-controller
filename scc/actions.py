1c42b51f99dfeacb91a9a84b1b65b30c9a44d0d2
kozec <kozec@kozec.com>
2016-05-01 09:45:12 +0200

    Just moving stuff around

--- scc/actions.py
+++ scc/actions.py
@@ -563,60 +563,6 @@ class ButtonAction(Action):
 			self._released = True
 
 
-class ChangeProfileAction(Action):
-	COMMAND = "profile"
-	
-	def __init__(self, profile):
-		Action.__init__(self, profile)
-		self.profile = profile
-	
-	def describe(self, context):
-		if self.name: return self.name
-		return _("Profile Change")
-	
-	
-	def to_string(self, multiline=False, pad=0):
-		return (" " * pad) + "%s('%s')" % (self.COMMAND, self.profile.encode('string_escape'))
-
-
-	def button_press(self, mapper):
-		pass
-	
-	def button_release(self, mapper):
-		# Can be executed only when releasing button
-		if mapper.change_profile_callback is None:
-			log.warning("Mapper can't change profile by controller action")
-		else:
-			mapper.change_profile_callback(self.profile)
-
-
-class ShellCommandAction(Action):
-	COMMAND = "shell"
-	
-	def __init__(self, command):
-		Action.__init__(self, command)
-		self.command = command
-	
-	def describe(self, context):
-		if self.name: return self.name
-		return _("Execute Command")
-	
-	
-	def to_string(self, multiline=False, pad=0):
-		return (" " * pad) + "%s('%s')" % (self.COMMAND, self.parameters[0].encode('string_escape'))
-	
-	
-	def button_press(self, mapper):
-		# Can be executed only when pressing button
-		if mapper.shell_command_callback is None:
-			log.warning("Mapper can't execute commands")
-		else:
-			mapper.shell_command_callback(self.command)
-	
-	def button_release(self, mapper):
-		pass
-
-
 class MultiAction(Action):
 	"""
 	Two or more actions executed at once.
@@ -922,3 +868,4 @@ ACTIONS["None"] = NoAction
 
 import scc.macros
 import scc.modifiers
+import scc.special_actions
---
+++ scc/special_actions.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python2
+"""
+SC Controller - Special Actions
+
+Moved from actions just to keep it shorter.
+"""
+from __future__ import unicode_literals
+
+from scc.actions import Action, NoAction, ButtonAction, ACTIONS, MOUSE_BUTTONS
+from scc.constants import FE_STICK, FE_TRIGGER, FE_PAD
+from scc.constants import LEFT, RIGHT, STICK, SCButtons
+
+
+import time, logging
+log = logging.getLogger("SActions")
+_ = lambda x : x
+
+
+class ChangeProfileAction(Action):
+	COMMAND = "profile"
+	
+	def __init__(self, profile):
+		Action.__init__(self, profile)
+		self.profile = profile
+	
+	def describe(self, context):
+		if self.name: return self.name
+		return _("Profile Change")
+	
+	
+	def to_string(self, multiline=False, pad=0):
+		return (" " * pad) + "%s('%s')" % (self.COMMAND, self.profile.encode('string_escape'))
+
+
+	def button_press(self, mapper):
+		pass
+	
+	def button_release(self, mapper):
+		# Can be executed only when releasing button
+		if mapper.change_profile_callback is None:
+			log.warning("Mapper can't change profile by controller action")
+		else:
+			mapper.change_profile_callback(self.profile)
+
+
+class ShellCommandAction(Action):
+	COMMAND = "shell"
+	
+	def __init__(self, command):
+		Action.__init__(self, command)
+		self.command = command
+	
+	def describe(self, context):
+		if self.name: return self.name
+		return _("Execute Command")
+	
+	
+	def to_string(self, multiline=False, pad=0):
+		return (" " * pad) + "%s('%s')" % (self.COMMAND, self.parameters[0].encode('string_escape'))
+	
+	
+	def button_press(self, mapper):
+		# Can be executed only when pressing button
+		if mapper.shell_command_callback is None:
+			log.warning("Mapper can't execute commands")
+		else:
+			mapper.shell_command_callback(self.command)
+	
+	def button_release(self, mapper):
+		pass
+
+
+# Add macros to ACTIONS dict
+for i in [ globals()[x] for x in dir() if hasattr(globals()[x], 'COMMAND') ]:
+	if i.COMMAND is not None:
+		ACTIONS[i.COMMAND] = i
