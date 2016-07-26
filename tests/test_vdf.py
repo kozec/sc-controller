4d7b36fbbdede324f1c6d6d1f1e5867a5715e0ec
kozec <kozec@kozec.com>
2016-07-26 14:57:51 +0200

    Added vdf import tests

--- tests/test_vdf.py
+++ tests/test_vdf.py
@@ -1,6 +1,7 @@
 from scc.lib.vdf import parse_vdf
+from scc.foreign.vdf import VDFProfile
 from cStringIO import StringIO
-import pytest
+import os, pytest
 
 class TestVDF(object):
 	""" Tests VDF parser """
@@ -37,9 +38,9 @@ class TestVDF(object):
 		""")
 		with pytest.raises(ValueError) as excinfo:
 			parsed = parse_vdf(sio)
-
-
-	def test_dict_without_key(self):
+	
+	
+	def test_unclosed_bracket(self):
 		"""
 		Tests if VDF parser throws exception when there is unclosed {
 		"""
@@ -55,3 +56,30 @@ class TestVDF(object):
 			parsed = parse_vdf(sio)
 	
 	
+	def test_too_many_brackets(self):
+		"""
+		Tests if VDF parser throws exception when there is } wihtout matching {
+		"""
+		sio = StringIO("""
+		"data"
+		{
+			"version" "3"
+			"more data" {
+				"version" "7"
+			}
+			}
+		}
+		""")
+		with pytest.raises(ValueError) as excinfo:
+			parsed = parse_vdf(sio)
+	
+	
+	def test_import(self):
+		"""
+		Tests if every *.vdf file in tests/vdfs can be imported.
+		"""
+		path = "tests/vdfs"
+		for f in os.listdir(path):
+			filename = os.path.join(path, f)
+			print "Testing import of '%s'" % (filename,)
+			VDFProfile().load(filename)
