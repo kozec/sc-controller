407cf2794b17be761fdf0efcc3f8a722f6ddef10
kozec <kozec@kozec.com>
2016-10-09 12:39:10 +0200

    Added code to track libuinput.so version and rebuild it if needed. Ref #139

--- scc/uinput.py
+++ scc/uinput.py
@@ -214,6 +214,16 @@ class UInput(object):
 			)
 		)
 		self._lib = ctypes.CDLL(lib)
+		
+		try:
+			if self._lib.uinput_module_version() != 1:
+				raise Exception()
+		except:
+			import sys
+			print >>sys.stderr, "Invalid native module version. Please, recompile 'libuinput.so'"
+			print >>sys.stderr, "If you are running sc-controller from source, you can do this by removing 'build' directory"
+			print >>sys.stderr, "and runinng 'run.sh' script"
+			raise Exception("Invalid native module version")
 
 		c_k		= (ctypes.c_uint16 * len(self._k))(*self._k)
 		c_a		= (ctypes.c_uint16 * len(self._a))(*self._a)
