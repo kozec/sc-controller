7799024b11da78ea95713ee0e36036224c94fbbe
kozec <kozec@kozec.com>
2016-10-13 13:31:20 +0200

    Gestures almost usable, with nice working cleanup method.

--- scc/gestures.py
+++ scc/gestures.py
@@ -10,6 +10,7 @@ from scc.actions import Action
 from scc.tools import circle_to_square, clamp
 from scc.constants import STICK_PAD_MIN, STICK_PAD_MAX
 from math import pi as PI, atan2, sqrt
+from itertools import groupby
 
 import logging
 log = logging.getLogger("Gestures")
@@ -23,7 +24,14 @@ class GestureDetector(Action):
 	GestureAction special action or "Gesture:" message from client,
 	it constructs instance of this class and leaves everything to it.
 	"""
-	MIN_MOVEMENT_SIZE = 500.0		# Difference in positions over both axes
+	# Minimal difference in positions over both axes
+	MIN_MOVEMENT_SIZE = 500.0
+	
+	# Constants used in GestureDetector.cleanup
+	DIRTY_ELS = ("412", "236", "698", "874", "214", "478", "896", "632",
+				 "284", "468", "826", "642", "286", "648", "824", "462")
+	SHORT = 0.3
+	
 	
 	def __init__(self, up_direction, vh_preference, on_finished):
 		Action.__init__(self)
@@ -45,7 +53,10 @@ class GestureDetector(Action):
 			if (x, y) == (0, 0):
 				# Released
 				self._enabled = False
-				self._on_finished(self, self._string)
+				self._on_finished(self, 
+					(self._string + "::" +
+					GestureDetector.cleanup(self._string)
+					))
 				return
 			else:
 				if self._old_pos is None:
@@ -56,31 +67,68 @@ class GestureDetector(Action):
 						angle = atan2(dy, dx) * 180.0 / PI
 						if angle < 0: angle += 360
 						self._string = "%s%s" % (
-							self._string, angle_to_direction(angle))
+							self._string, GestureDetector.angle_to_direction(angle))
 						self._old_pos = x, y
-
-
-def angle_to_direction(angle):
-	"""
-	Translates direction expressed in degrees (where 0.0 goes left and 90.0 up)
-	to direction expressed using numbers of numpad (because that's as good as
-	anything else for my purposes)
-	"""
-	B = 30
-	if angle > 360 - B or angle < B:
-		return 4	# Left
-	elif angle > B and angle < 90 - B:
-		return 1	# Left-Down
-	elif angle > 90 - B and angle < 90 + B:
-		return 2	# Down
-	elif angle > 90 + B and angle < 180 - B:
-		return 3	# Right-Down
-	elif angle > 180 - B and angle < 180 + B:
-		return 6	# Rigth
-	elif angle > 180 + B and angle < 270 - B:
-		return 9	# Rigth-Up
-	elif angle > 270 - B and angle < 270 + B:
-		return 8	# Up
-	else:
-		return 7	# Left-Up
 	
+	
+	@staticmethod
+	def cleanup(dirty):
+		"""
+		This does magic.
+		
+		1st, it breaks gesture into consecutive strings of same characters.
+		2nd, it find longest such string and removes every other string that is
+		     shorter than rougly 1/3 of longest size
+		3rd, it reassemles string back, removing all repeating characters
+		4rd, as special treatement, removes all cases when there is diagonal
+		     motion detected between two straight movements, to make detection
+		     of "L-like" movements better.
+		
+		Doing this had best results in making sure that I can get same gesture
+		string for eveyr time when I attempt to do same gesture.
+		"""
+		cur, split = [], []
+		last, longest = None, 0
+		for ch in dirty:
+			if ch != last:
+				if len(cur) > 0:
+					longest = max(longest, len(cur))
+					split.append("".join(cur))
+					cur = []
+					last = ch
+			cur.append(ch)
+		
+		req_len = max(1, int(longest * GestureDetector.SHORT))
+		split = ( x[0] for x in split if len(x) > req_len )
+		almost_clean = "".join(( x[0] for x in groupby(split) ))
+		for dl in GestureDetector.DIRTY_ELS:
+			while dl in almost_clean:
+				almost_clean = almost_clean.replace(dl, "")
+		
+		return "[%s]%s" % (longest, almost_clean)
+	
+	
+	@staticmethod
+	def angle_to_direction(angle):
+		"""
+		Translates direction expressed in degrees (where 0.0 goes left and 90.0 up)
+		to direction expressed using numbers of numpad (because that's as good as
+		anything else for my purposes)
+		"""
+		B = 45
+		if angle > 360 - B or angle < B:
+			return 4	# Left
+		elif angle > B and angle < 90 - B:
+			return 1	# Left-Down
+		elif angle > 90 - B and angle < 90 + B:
+			return 2	# Down
+		elif angle > 90 + B and angle < 180 - B:
+			return 3	# Right-Down
+		elif angle > 180 - B and angle < 180 + B:
+			return 6	# Rigth
+		elif angle > 180 + B and angle < 270 - B:
+			return 9	# Rigth-Up
+		elif angle > 270 - B and angle < 270 + B:
+			return 8	# Up
+		else:
+			return 7	# Left-Up
