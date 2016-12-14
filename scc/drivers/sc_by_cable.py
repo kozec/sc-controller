2480d02d4c15fdc736dcf3b9fd3d119412b965cc
kozec <kozec@kozec.com>
2016-12-14 23:38:48 +0100

    Fix: Using wired inputs as timer is too slow. Fixes #129

--- scc/drivers/sc_by_cable.py
+++ scc/drivers/sc_by_cable.py
@@ -4,12 +4,14 @@ from scc.lib import usb1
 from scc.drivers.usb import USBDevice, register_hotplug_device
 from sc_dongle import ControllerInput, SCI_NULL, TUP_FORMAT
 from sc_dongle import SCStatus, SCPacketType, SCConfigType, SCController
+from threading import Thread, Lock
 import struct, time, logging
 
 VENDOR_ID = 0x28de
 PRODUCT_ID = 0x1102
 ENDPOINT = 3
 CONTROLIDX = 2
+TIMER_INTERVAL = 0.01
 
 log = logging.getLogger("SCCable")
 
@@ -28,6 +30,9 @@ class SCByCable(USBDevice, SCController):
 		self.daemon = daemon
 		self._ready = False
 		self._last_tup = None
+		self._lock = Lock()
+		self._thread = Thread(target = self._timer)
+		self._thread.start()
 		
 		self.claim_by(klass=3, subclass=0, protocol=0)
 		self.read_serial()
@@ -45,25 +50,31 @@ class SCByCable(USBDevice, SCController):
 	
 	def _wait_input(self, endpoint, data):
 		tup = ControllerInput._make(struct.unpack(TUP_FORMAT, data))
-		if not self._ready:
-			self.daemon.add_controller(self)
-			self.configure()
-			self._ready = True
-		if tup.status == SCStatus.INPUT:
-			self._last_tup = tup
-			self.input(tup)
-		elif tup.status == SCStatus.IDLE:
-			# TODO: Probably use real timer here, wired controller sends IDLE
-			# only once per second
-			if self.get_mapper():
-				# TODO: Also remove run_scheduled at that time
-				self.get_mapper().run_scheduled(time.time())
+		with self._lock:
+			if not self._ready:
+				self.daemon.add_controller(self)
+				self.configure()
+				self._ready = True
+			if tup.status == SCStatus.INPUT:
+				self._last_tup = tup
+				self.input(tup)
+			elif tup.status == SCStatus.IDLE:
+				pass
+	
+	
+	def _timer(self):
+		while self._timer:
+			time.sleep(TIMER_INTERVAL)
+			with self._lock:
+				if self.get_mapper():
+					self.get_mapper().run_scheduled(time.time())
 	
 	
 	def close(self):
 		if self._ready:
 			self.daemon.remove_controller(self)
 			self._ready = False
+		self._timer = None
 		USBDevice.close(self)
 	
 	
