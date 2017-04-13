926c2a9a6b94576380ac891d56b63ac4c149d2f2
kozec <kozec@kozec.com>
2017-04-13 21:07:36 +0200

    ... and radial menu has feedback too

--- scc/osd/radial_menu.py
+++ scc/osd/radial_menu.py
@@ -149,8 +149,6 @@ class RadialMenu(Menu):
 	
 	
 	def select(self, index):
-		if self.feedback:
-			self.controller.feedback(*self.feedback)
 		self._selected = self.items[index]
 	
 	
@@ -184,6 +182,8 @@ class RadialMenu(Menu):
 				for i in self.items:
 					if abs(degdiff(i.a, angle)) < half_width:
 						if self._selected != i:
+							if self.feedback and self.controller:
+								self.controller.feedback(*self.feedback)
 							self._selected = i
 							self.b.hilight({
 								"menuitem_" + i.id : "#" + self.config["osd_colors"]["menuitem_hilight"],
