7ffa8b01055319da1bd8143e0f69cb58593a9736
kozec <kozec@kozec.com>
2018-12-02 09:44:27 +0100

    forgotten file

---
+++ src/daemon/x11.c
@@ -0,0 +1,43 @@
+#define LOG_TAG "Daemon"
+#include "scc/utils/logging.h"
+#include "scc/utils/assert.h"
+#include "daemon.h"
+
+static void* x11display;
+#ifndef NO_X11
+#include "X11/Xlib.h"
+
+void sccd_x11_init() {
+	x11display = (void*)XOpenDisplay(NULL);
+	if (x11display == NULL) {
+		WARN("Failed to connect to XServer. Some functionality will be unavailable.");
+	} else {
+		LOG("Connected to XServer %s", DisplayString(x11display));
+	}
+}
+
+void sccd_x11_close() {
+	if (x11display != NULL) {
+		XCloseDisplay(x11display);
+		x11display = NULL;
+	}
+}
+
+#else
+
+void sccd_x11_init() {
+	x11display = NULL;
+}
+
+void sccd_x11_close() {
+}
+
+#endif
+
+/**
+ * Returns open connection to X11 display.
+ * Returns NULL on Windows, Android, or on *nix if connection to display failed
+ */
+void* sccd_x11_get_display() {
+	return x11display;
+}
