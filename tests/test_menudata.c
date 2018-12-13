c1c3ad1a5ad5069cb4afa50e401bfb825f03d40f
kozec <kozec@kozec.com>
2018-12-13 22:52:43 +0100

    Fixed menu & profile tests

--- tests/test_menudata.c
+++ tests/test_menudata.c
@@ -11,22 +11,21 @@ void test_loading(CuTest* tc) {
 	MenuData* m = scc_menudata_from_json("../default_menus/Default.menu", NULL);
 	assert(tc, m != NULL);
 	
-	MenuItem* it = scc_menudata_get_by_id(m, "keyboard_item");
+	MenuItem* it = scc_menudata_get_by_id(m, "turnoff_item");
 	assert(tc, it != NULL);
-	assert(tc, strcmp(it->icon, "system/keyboard") == 0);
+	assert(tc, strcmp(it->icon, "system/turn-off") == 0);
 	
 	it = scc_menudata_get_by_id(m, "separator-after-profile-list");
 	assert(tc, it != NULL);
 	assert(tc, it->rows == 5);	// checks default
 	assert(tc, it->type == MI_SEPARATOR);
 	
-	it = scc_menudata_get_by_id(m, "auto-id-2");
-	assert(tc, it != NULL);
-	assert(tc, it->type == MI_GENERATOR);
-	assert(tc, it->rows == 3);
-	assert(tc, strcmp(it->generator, "recent") == 0);
-	
-	// RC_REL(p);
+	// TODO: Reenable this, there is currently no generator there
+	// it = scc_menudata_get_by_id(m, "auto-id-2");
+	// assert(tc, it != NULL);
+	// assert(tc, it->type == MI_GENERATOR);
+	// assert(tc, it->rows == 3);
+	// assert(tc, strcmp(it->generator, "recent") == 0);
 }
 
 
